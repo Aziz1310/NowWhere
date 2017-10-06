43d3191e5685f80faeb40724ed455353e485d055
maher <maher.gafsi@esprit.tn>
2017-10-06 21:59:26 +0100

    Design : sign in activity sign up activity home activity

--- app/src/main/java/com/maher/nowhere/mainActivity/fragments/Categories.java
+++ app/src/main/java/com/maher/nowhere/mainActivity/fragments/Categories.java
@@ -1,4 +1,4 @@
-package com.maher.nowhere;
+package com.maher.nowhere.mainActivity.fragments;
 
 import android.content.Context;
 import android.net.Uri;
@@ -8,16 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.maher.nowhere.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link Weeklik.OnFragmentInteractionListener} interface
+ * {@link Categories.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link Weeklik#newInstance} factory method to
+ * Use the {@link Categories#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Weeklik extends Fragment {
+public class Categories extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +31,7 @@ public class Weeklik extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public Weeklik() {
+    public Categories() {
         // Required empty public constructor
     }
 
@@ -39,11 +41,11 @@ public class Weeklik extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Weeklik.
+     * @return A new instance of fragment Categories.
      */
     // TODO: Rename and change types and number of parameters
-    public static Weeklik newInstance(String param1, String param2) {
-        Weeklik fragment = new Weeklik();
+    public static Categories newInstance(String param1, String param2) {
+        Categories fragment = new Categories();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +66,7 @@ public class Weeklik extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weeklik, container, false);
+        return inflater.inflate(R.layout.fragment_categories, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
